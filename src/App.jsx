9f624825fd16200bae4a377487ed025645e6867d
Shapel Nahyrych <shapel.nahyrych@gmail.com>
2026-02-24 21:59:00 +0000

    TextArea

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import CursorTracker from "./components/CursorTracker/CursorTracker";
-import DragElement from "./components/DragElement/DragElement";
+import TextAreaAutoSave from "./components/TextAreaAutoSave/TextAreaAutoSave";
+
 
 
 function App() {
@@ -7,10 +7,7 @@ function App() {
   return (
     <>
       
-      <DragElement text='1' left={325} top={25} />
-      <DragElement text='2' left={125} top={125}/>
-      <DragElement text='3' left={225} top={225} />
-      <DragElement/>
+      <TextAreaAutoSave />
       
     </>
   );
---
+++ src/components/TextAreaAutoSave/TextAreaAutoSave.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const TextAreaAutoSave = () => {
+  const [text, setText] = useState("");
+  const changeText = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    const saveText = localStorage.getItem("saveText");
+    if (saveText) {
+      setText(saveText);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("saveText", text);
+  }, [text]);
+
+  return (
+    <section>
+      <h3>auto save text area</h3>
+      <textarea
+        name=""
+        id=""
+        rows="5"
+        cols="40"
+        value={text}
+        onChange={changeText}
+      ></textarea>
+    </section>
+  );
+};
+
+export default TextAreaAutoSave;
+//  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam facilis soluta dolorum ullam sit porro laboriosam, unde ut atque natus sapiente assumenda nisi velit? Temporibus!
